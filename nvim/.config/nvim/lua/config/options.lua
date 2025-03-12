75dbf2762968fc022811c229e142338f678a9ae8
Carlos J. Torres <vlaadbrain@gmail.com>
2025-03-11 21:59:11 -0400

    update options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,54 +3,14 @@
 -- Add any additional options here
 
 vim.g.have_nerd_font = true
--- Make line numbers default
-vim.opt.number = true
-vim.opt.numberwidth = 2
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = "a"
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Enable break indent
+vim.opt.numberwidth = 1
+-- vim.opt.relativenumber = true
 vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
+vim.opt.updatetime = 50
+vim.opt.list = false
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
--- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 90
+vim.g.autoformat = true
 
 -- vim: ts=2 sts=2 sw=2 et
