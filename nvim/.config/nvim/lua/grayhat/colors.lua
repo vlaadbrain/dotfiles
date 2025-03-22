4d0fb2928dc5d09c7b9e6f2d3c272d32426142c6
Carlos J. Torres <vlaadbrain@gmail.com>
2025-03-21 20:48:15 -0400

    solarized theme

--- nvim/.config/nvim/lua/grayhat/colors.lua
+++ nvim/.config/nvim/lua/grayhat/colors.lua
@@ -1,7 +1,9 @@
 require('solarized').setup({})
 
 vim.o.termguicolors = true
-vim.cmd.colorscheme 'solarized-osaka'
+--vim.cmd.colorscheme 'moonlight'
+vim.cmd.colorscheme 'solarized'
+--vim.cmd.colorscheme 'solarized-osaka'
 vim.opt.termguicolors = true
 vim.o.background = 'dark'
 
