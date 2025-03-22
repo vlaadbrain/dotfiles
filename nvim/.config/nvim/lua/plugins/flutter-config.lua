2903676f4032656858e5241c9087d194f269f769
Carlos J. Torres <vlaadbrain@gmail.com>
2025-03-21 20:51:45 -0400

    add flutter-tools and dartls plugins

---
+++ nvim/.config/nvim/lua/plugins/flutter-config.lua
@@ -0,0 +1,28 @@
+return {
+  'akinsho/flutter-tools.nvim',
+  lazy = false,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim', -- optional for vim.ui.select
+  },
+  config = function()
+    require('flutter-tools').setup {
+      decorations = {
+        statusline = {
+          app_version = true,
+          device = true,
+        },
+      },
+      dev_tools = {
+        autostart = true, -- autostart devtools server if not detected
+        auto_open_browser = true, -- Automatically opens devtools in the browser
+        open_cmd = 'splitbelow ++bo',
+      },
+      lsp = {
+        color = { -- show the derived colours for dart variables
+          enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+        },
+      },
+    }
+  end,
+}
