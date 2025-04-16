f094f8e61aba7f2f264903adc383f07b3f519786
Carlos J. Torres <vlaadbrain@gmail.com>
2025-04-16 09:01:12 -0400

    update lock file, use single colors plugin loader

---
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    'maxmx03/solarized.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    'gbprod/nord.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+    end,
+  },
+  {
+    'mhartington/oceanic-next',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+    end,
+  }
+}
