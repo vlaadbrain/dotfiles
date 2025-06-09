390059c4db9ba2c47ee87e3ddcae75705b4265cf
Carlos J. Torres <vlaadbrain@gmail.com>
2025-06-09 12:43:16 -0400

    restow when stowing :)

--- stow-setup.sh
+++ stow-setup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-stow -t ~ git
-stow -t ~ 1Password
-stow -t ~ ghostty
-stow -t ~ iterm2
-stow -t ~ eza
-stow -t ~ fzf
-stow -t ~ npm
-stow -t ~ tmux
-stow -t ~ zsh
-stow -t ~ oh-my-zsh
-stow -t ~ nvim
-stow -t ~ x11
-stow -t ~ mpd
+stow -R -t ~ git
+stow -R -t ~ 1Password
+stow -R -t ~ ghostty
+stow -R -t ~ iterm2
+stow -R -t ~ eza
+stow -R -t ~ fzf
+stow -R -t ~ npm
+stow -R -t ~ tmux
+stow -R -t ~ zsh
+stow -R -t ~ oh-my-zsh
+stow -R -t ~ nvim
+stow -R -t ~ x11
+stow -R -t ~ mpd
