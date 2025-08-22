b3d8b97404d9fa0182f103fb394373ca5c487163
Carlos J. Torres <vlaadbrain@gmail.com>
2025-08-22 12:15:06 -0400

    i like the highlighted yank

--- nvim/.config/nvim/lua/grayhat/init.lua
+++ nvim/.config/nvim/lua/grayhat/init.lua
@@ -3,3 +3,9 @@ require("grayhat.loading")
 require("grayhat.remap")
 require("grayhat.set")
 require("grayhat.status")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function ()
+    vim.highlight.on_yank({higroup="IncSearch", timeout=300})
+  end,
+})
