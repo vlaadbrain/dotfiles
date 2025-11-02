ecb7e12299e6113673ee0a14911800bc1d78a2ff
Carlos J. Torres <vlaadbrain@gmail.com>
2025-11-01 23:59:58 -0400

    conform changes

--- nvim/.config/nvim/lua/grayhat/colors.lua
+++ nvim/.config/nvim/lua/grayhat/colors.lua
@@ -2,18 +2,17 @@
   borders = true
 })
 ]]
-require('solarized').setup({})
-local config = require 'solarized.config'
-local palette = require 'solarized.palette'
+require("solarized").setup({})
+local config = require("solarized.config")
+local palette = require("solarized.palette")
 local colors = palette[config.palette]
 
 vim.o.termguicolors = true
 --vim.cmd.colorscheme 'moonlight'
-vim.cmd.colorscheme 'solarized'
+vim.cmd.colorscheme("solarized")
 --vim.cmd.colorscheme 'solarized-osaka'
 vim.opt.termguicolors = true
-vim.o.background = 'dark'
-vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = colors.red, bold = true })
+vim.o.background = "dark"
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = colors.red, bold = true })
 --vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = colors.cyan })
 --vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = colors.blue })
-
