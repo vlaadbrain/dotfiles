17f112b4490074e59e4925960ea28217593981fa
Carlos J. Torres <vlaadbrain@gmail.com>
2025-11-01 23:58:35 -0400

    fix typo in magic script

--- source-erer.sh
+++ source-erer.sh
@@ -1,6 +1,6 @@
 export ANTHROPIC_API_KEY='op://Private/Anthropic API/x-api-key'
 export GITHUB_TOKEN='op://Private/GitHub/token'
-export ATLASSIAN_TOKEN='op://Private/Atlassian/token''
+export ATLASSIAN_TOKEN='op://Private/Atlassian/token'
 
 export YEXT_TOKEN=${GITHUB_TOKEN}
 export GIT_TOKEN=${GITHUB_TOKEN}
