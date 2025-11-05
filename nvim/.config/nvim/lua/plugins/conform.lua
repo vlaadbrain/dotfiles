6a71a022cedb8d3edcd66dbec172bd3af5169f15
Carlos J. Torres <vlaadbrain@gmail.com>
2025-11-04 22:49:15 -0500

    conform should be less eager with C.

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,21 +1,33 @@
 return {
 	"stevearc/conform.nvim",
 	opts = {
+		format_on_save = false,
 		formatters_by_ft = {
 			lua = { "stylua" },
 			kotlin = { "ktlint" },
 			dart = { "dart_format" },
-			cpp = { "clang-format" },
-			c = { "clang-format" },
+			cpp = { "clang_format" },
+			c = { "clang_format" },
 		},
 		formatters = {
 			dart_format = {
 				command = "dart",
 				args = { "format", "$FILENAME" },
 			},
-		},
-		format_on_save = {
-			lsp_format = "fallback",
+			clang_format = {
+				prepend_args = { "--style=file" },
+				args = function()
+					local shiftwidth = vim.api.nvim_get_option("shiftwidth")
+					local expandtab = vim.api.nvim_get_option("expandtab")
+					return {
+						"--style={BasedOnStyle: llvm, IndentWidth: "
+							.. shiftwidth
+							.. ", UseTab: "
+							.. (expandtab and "Never" or "Always")
+							.. "}",
+					}
+				end,
+			},
 		},
 	},
 }
