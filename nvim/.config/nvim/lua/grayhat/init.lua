492749eb8ff452733955308b2894a9087fb7074f
Carlos J. Torres <vlaadbrain@gmail.com>
2026-01-27 11:27:21 -0500

    go to the latest line i was on for that file ShaDa

--- nvim/.config/nvim/lua/grayhat/init.lua
+++ nvim/.config/nvim/lua/grayhat/init.lua
@@ -10,3 +10,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 300 })
 	end,
 })
+
+local remember_cursor_group = vim.api.nvim_create_augroup("vimrc-remember-cursor-position", { clear = true })
+vim.api.nvim_create_autocmd("BufReadPost", {
+  desc = "Restore cursor position after opening file", -- Optional description
+  group = remember_cursor_group,
+  pattern = "*", -- Apply to all files
+  callback = function()
+    -- Lua equivalent of the Vimscript logic
+    if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+      vim.cmd("normal! g`\"")
+    end
+  end,
+})
